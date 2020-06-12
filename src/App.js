12b10f981aa6b099b376dab50548c9b586a4694b
Qingtian Mei <39237932+frankmei1@users.noreply.github.com>
2020-06-11 22:45:21 -0400

    Joined Group Page Set up

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Header from './components/header.js'
 import Home from './components/home.js'
 import MyProfile from './components/myProfile.js'
 import SubGroupCard from './components/subGroupCard.js'
+import JoinGroup from './components/joinGroup.js'
+
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
@@ -22,6 +24,7 @@ function App() {
              <Route path="/home" render={ ()=> <Home post={"groupData"}/>}/>
              <Route path="/myProfile" render={ ()=> <MyProfile post={"groupData"}/>}/>
              <Route path='/learn more' component = {SubGroupCard}/>    
+             <Route path='/joinGroup'render={ ()=> <JoinGroup post={"groupData"}/>}/>    
              <Route path="/" render={ ()=> <Home post={"groupData"}/>}/>
              <Route component={Error}/>
            </Switch>
@@ -37,6 +40,7 @@ const sections = [
   { title: 'About Us', url: '#' },//app intro, might be merged to "home" tab
   { title: 'Challenges', url: '/groups' },
   { title: 'My Profile', url: '/myProfile' },
+  { title: 'Joined Group', url: '/joinGroup' },
   { title: 'Setting', url: '#' }
 ];
 
--- src/components/joinGroup.js
+++ src/components/joinGroup.js
@@ -1,29 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, BrowserRouter } from 'react-router-dom';
-import subGroupCard from './subGroupCard';
-import { Switch } from '@material-ui/core';
 
-const GroupCard = ({ group }) => {
+const JoinGroup = ({ group }) => {
   return (
     <div className='card'>
       <div className='card-body'>
-        <h2 className='card-title'>{`#${group.challenge}`}</h2>
+        <h2 className='card-title'>Place Holder</h2>
       </div>
       <ul className='list-group list-group-flush' >
-        <li className='list-group-item'>{`Enrollment: ${group.enrollment}`}</li>
+        <li className='list-group-item'>One</li>
       </ul>
-      <BrowserRouter>
-        <div>
-        <Route path="/learn more" render={ ()=> <subGroupCard group = {group}/> }/>
-        </div>
-      </BrowserRouter>
     </div>
   );
 };
 
-GroupCard.propTypes = {
+JoinGroup.propTypes = {
   group: PropTypes.object.isRequired
 }
 
-export default GroupCard;
+export default JoinGroup;
