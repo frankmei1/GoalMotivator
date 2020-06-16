41dc7beb1fb28c682318465ffb32994cec62f8bc
minkwonchoi <56175091+minkwonchoi@users.noreply.github.com>
2020-06-16 10:34:54 -0400

    Update ChrisBio.js

--- Bio/aboutdemo/components/ChrisBio.js
+++ Bio/aboutdemo/components/ChrisBio.js
@@ -7,8 +7,12 @@ export default function ChrisBio({ navigation }) {
   return (
     <View style={{ flex: 3, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Chris Choi</Text>
+
+      <Text>Brandeis Universiry, Physics and Computer Science Major</Text>
     </View>
-    <View style={{ flex: 2, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 3, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Chris Choi</Text>
+
       <Text>Brandeis Universiry, Physics and Computer Science Major</Text>
     </View>
   );
