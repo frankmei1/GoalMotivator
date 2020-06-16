93de09bf25fc6dd6e48dd78eea219e6f031df74a
minkwonchoi <56175091+minkwonchoi@users.noreply.github.com>
2020-06-16 10:03:07 -0400

    AddedBioDemo

---
+++ Bio/aboutdemo/components/ChrisBio.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+export default function ChrisBio({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Chris Choi</Text>
+
+
+    </View>
+  );
+}
