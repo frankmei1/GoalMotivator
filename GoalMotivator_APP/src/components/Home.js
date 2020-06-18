a83cafedfcd5f1a1a9b8aa80b4d326be1ddc84d7
minkwonchoi <56175091+minkwonchoi@users.noreply.github.com>
2020-06-18 10:04:44 -0400

    flatlist1

--- GoalMotivator_APP/src/components/Home.js
+++ GoalMotivator_APP/src/components/Home.js
@@ -1,26 +1,86 @@
-import * as React from 'react';
-import { Text, View, Button } from 'react-native';
+import React from 'react';
+import {
+  SafeAreaView,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+  Text,
+} from 'react-native';
+import Constants from 'expo-constants';
 
-export default function Home({ navigation }) {
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'My Page',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Challenges',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Prove',
+  },
+];
+
+function Item({ id, title, selected, onSelect }) {
   return (
-    <View >
-      <Button
-        title="Details"
-        onPress={() => {
-          /* 1. Navigate to the Details route with params */
-          navigation.navigate('Details', {
-            name:"xz",
-            age:19,
-          });
-        }}
-      />
-      <Button
-        title="About The Developers"
-        onPress={() => {
-          /* 1. Navigate to the Details route with params */
-          navigation.navigate('About Us');
-        }}
-      />
-    </View>
-  )
+    <TouchableOpacity
+      onPress={() => onSelect(id)}
+      style={[
+        styles.item,
+        { backgroundColor: selected ? '#6e3b6e' : '#f9c2ff' },
+      ]}
+    >
+      <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
+  );
 }
+
+export default function Home() {
+  const [selected, setSelected] = React.useState(new Map());
+
+  const onSelect = React.useCallback(
+    id => {
+      const newSelected = new Map(selected);
+      newSelected.set(id, !selected.get(id));
+
+      setSelected(newSelected);
+    },
+    [selected],
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) => (
+          <Item
+            id={item.id}
+            title={item.title}
+            selected={!!selected.get(item.id)}
+            onSelect={onSelect}
+          />
+        )}
+        keyExtractor={item => item.id}
+        extraData={selected}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+  item: {
+    backgroundColor: '#90ee90',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
